20eb6ef4f9ee5619f0585fc5d6a01c575cd68fe5
Andres Buritica <andres@thelinuxkid.com>
2012-04-24 01:03:30 -0700

    Add address_information.verify

---
+++ pyusps/address_information.py
@@ -0,0 +1,141 @@
+import urllib2
+import urllib
+
+from lxml import etree
+from collections import OrderedDict
+
+api_url = 'http://production.shippingapis.com/ShippingAPI.dll'
+address_max = 5
+
+def _raise_if_error(res):
+    root = res.getroot()
+    if root.tag == 'Error':
+        num = root.find('Number')
+        desc = root.find('Description')
+        raise ValueError(
+            '{num}: {desc}'.format(
+                num=num.text,
+                desc=desc.text,
+                )
+            )
+
+def _parse_response(res):
+    results = []
+    for address in res.iterfind('Address'):
+        result = OrderedDict()
+        for child in address.iterchildren():
+            # elements are yielded in order
+            name = child.tag.lower()
+            # More user-friendly names for street
+            # attributes
+            if name == 'address2':
+                name = 'address'
+            elif name == 'address1':
+                name = 'address_extended'
+            elif name == 'firmname':
+                name = 'firm_name'
+            result[name] = child.text
+        results.append(result)
+
+    return results
+
+def _get_response(xml):
+    params = OrderedDict([
+            ('API', 'Verify'),
+            ('XML', etree.tostring(xml)),
+            ])
+    url = '{api_url}?{params}'.format(
+        api_url=api_url,
+        params=urllib.urlencode(params),
+        )
+
+    res = urllib2.urlopen(url)
+    res = etree.parse(res)
+
+    return res
+
+def _create_xml(
+    user_id,
+    addresses,
+    ):
+    root = etree.Element('AddressValidateRequest', USERID=user_id)
+
+    if len(addresses) > address_max:
+        # Raise here. The Verify API will not return an error. It will
+        # just return the first 5 results
+        raise ValueError(
+            'Only {address_max} addresses are allowed per '
+            'request'.format(
+                address_max=address_max,
+                )
+            )
+
+    for i,_address in enumerate(addresses):
+        address = _address['address']
+        city = _address['city']
+        state= _address['state']
+        zip_code = _address.get('zip_code', None)
+        address_extended = _address.get('address_extended', None)
+        firm_name = _address.get('firm_name', None)
+        urbanization = _address.get('urbanization', None)
+
+        address_el = etree.Element('Address', ID=str(i))
+        root.append(address_el)
+
+        # Documentation says this tag is required but tests
+        # show it isn't
+        if firm_name is not None:
+            firm_name_el = etree.Element('FirmName')
+            firm_name_el.text = firm_name
+            address_el.append(firm_name_el)
+
+        address_1_el = etree.Element('Address1')
+        if address_extended is not None:
+            address_1_el.text = address_extended
+        address_el.append(address_1_el)
+
+        address_2_el = etree.Element('Address2')
+        address_2_el.text = address
+        address_el.append(address_2_el)
+
+        city_el = etree.Element('City')
+        city_el.text = city
+        address_el.append(city_el)
+
+        state_el = etree.Element('State')
+        if state is not None:
+            state_el.text = state
+        address_el.append(state_el)
+
+        if urbanization is not None:
+            urbanization_el = etree.Element('Urbanization')
+            urbanization_el.text = urbanization
+            address_el.append(urbanization_el)
+
+        zip5 = None
+        zip4 = None
+        if zip_code is not None:
+            zip5 = zip_code[:5]
+            zip4 = zip_code[5:]
+            if zip4.startswith('-'):
+                zip4 = zip4[1:]
+
+        zip5_el = etree.Element('Zip5')
+        if zip5 is not None:
+            zip5_el.text = zip5
+        address_el.append(zip5_el)
+
+        zip4_el = etree.Element('Zip4')
+        if zip4 is not None:
+            zip4_el.text = zip4
+        address_el.append(zip4_el)
+
+    return root
+
+def verify(user_id, addresses):
+    xml = _create_xml(user_id, addresses)
+    res = _get_response(xml)
+    _raise_if_error(res)
+    res = _parse_response(res)
+
+    return res
---
+++ pyusps/test/test_address_information.py
@@ -0,0 +1,336 @@
+import fudge
+
+from collections import OrderedDict
+from nose.tools import eq_ as eq
+from cStringIO import StringIO
+
+from pyusps.address_information import verify
+from pyusps.test.util import assert_raises
+
+@fudge.patch('urllib2.urlopen')
+def test_verify_simple(fake_urlopen):
+    fake_urlopen = fake_urlopen.expects_call()
+    req = """http://production.shippingapis.com/ShippingAPI.dll?API=Verify&XML=%3CAddressValidateRequest+USERID%3D%22foo_id%22%3E%3CAddress+ID%3D%220%22%3E%3CAddress1%2F%3E%3CAddress2%3E6406+Ivy+Lane%3C%2FAddress2%3E%3CCity%3EGreenbelt%3C%2FCity%3E%3CState%3EMD%3C%2FState%3E%3CZip5%2F%3E%3CZip4%2F%3E%3C%2FAddress%3E%3C%2FAddressValidateRequest%3E"""
+    fake_urlopen = fake_urlopen.with_args(req)
+    res = StringIO("""<?xml version="1.0"?>
+<AddressValidateResponse><Address ID="0"><Address2>6406 IVY LN</Address2><City>GREENBELT</City><State>MD</State><Zip5>20770</Zip5><Zip4>1441</Zip4></Address></AddressValidateResponse>""")
+    fake_urlopen.returns(res)
+
+    addresses = [
+        OrderedDict([
+                ('address', '6406 Ivy Lane'),
+                ('city', 'Greenbelt'),
+                ('state', 'MD'),
+                ])
+        ]
+    res = verify(
+        'foo_id',
+        addresses,
+        )
+
+    expected = [
+        OrderedDict([
+                ('address', '6406 IVY LN'),
+                ('city', 'GREENBELT'),
+                ('state', 'MD'),
+                ('zip5', '20770'),
+                ('zip4', '1441'),
+                ])
+        ]
+    eq(res, expected)
+
+@fudge.patch('urllib2.urlopen')
+def test_verify_zip5(fake_urlopen):
+    fake_urlopen = fake_urlopen.expects_call()
+    req = """http://production.shippingapis.com/ShippingAPI.dll?API=Verify&XML=%3CAddressValidateRequest+USERID%3D%22foo_id%22%3E%3CAddress+ID%3D%220%22%3E%3CAddress1%2F%3E%3CAddress2%3E6406+Ivy+Lane%3C%2FAddress2%3E%3CCity%3EGreenbelt%3C%2FCity%3E%3CState%3EMD%3C%2FState%3E%3CZip5%3E20770%3C%2FZip5%3E%3CZip4%3E%3C%2FZip4%3E%3C%2FAddress%3E%3C%2FAddressValidateRequest%3E"""
+    fake_urlopen = fake_urlopen.with_args(req)
+    res = StringIO("""<?xml version="1.0"?>
+<AddressValidateResponse><Address ID="0"><Address2>6406 IVY LN</Address2><City>GREENBELT</City><State>MD</State><Zip5>20770</Zip5><Zip4>1441</Zip4></Address></AddressValidateResponse>""")
+    fake_urlopen.returns(res)
+
+    addresses = [
+        OrderedDict([
+                ('address', '6406 Ivy Lane'),
+                ('city', 'Greenbelt'),
+                ('state', 'MD'),
+                ('zip_code', '20770'),
+                ])
+        ]
+    res = verify(
+        'foo_id',
+        addresses,
+        )
+
+    expected = [
+        OrderedDict([
+                ('address', '6406 IVY LN'),
+                ('city', 'GREENBELT'),
+                ('state', 'MD'),
+                ('zip5', '20770'),
+                ('zip4', '1441'),
+                ])
+        ]
+    eq(res, expected)
+
+@fudge.patch('urllib2.urlopen')
+def test_verify_zip_both(fake_urlopen):
+    fake_urlopen = fake_urlopen.expects_call()
+    req = """http://production.shippingapis.com/ShippingAPI.dll?API=Verify&XML=%3CAddressValidateRequest+USERID%3D%22foo_id%22%3E%3CAddress+ID%3D%220%22%3E%3CAddress1%2F%3E%3CAddress2%3E6406+Ivy+Lane%3C%2FAddress2%3E%3CCity%3EGreenbelt%3C%2FCity%3E%3CState%3EMD%3C%2FState%3E%3CZip5%3E20770%3C%2FZip5%3E%3CZip4%3E1441%3C%2FZip4%3E%3C%2FAddress%3E%3C%2FAddressValidateRequest%3E"""
+    fake_urlopen = fake_urlopen.with_args(req)
+    res = StringIO("""<?xml version="1.0"?>
+<AddressValidateResponse><Address ID="0"><Address2>6406 IVY LN</Address2><City>GREENBELT</City><State>MD</State><Zip5>20770</Zip5><Zip4>1441</Zip4></Address></AddressValidateResponse>""")
+    fake_urlopen.returns(res)
+
+    addresses = [
+        OrderedDict([
+                ('address', '6406 Ivy Lane'),
+                ('city', 'Greenbelt'),
+                ('state', 'MD'),
+                ('zip_code', '207701441'),
+                ])
+        ]
+    res = verify(
+        'foo_id',
+        addresses,
+        )
+
+    expected = [
+        OrderedDict([
+                ('address', '6406 IVY LN'),
+                ('city', 'GREENBELT'),
+                ('state', 'MD'),
+                ('zip5', '20770'),
+                ('zip4', '1441'),
+                ])
+        ]
+    eq(res, expected)
+
+@fudge.patch('urllib2.urlopen')
+def test_verify_zip_dash(fake_urlopen):
+    fake_urlopen = fake_urlopen.expects_call()
+    req = """http://production.shippingapis.com/ShippingAPI.dll?API=Verify&XML=%3CAddressValidateRequest+USERID%3D%22foo_id%22%3E%3CAddress+ID%3D%220%22%3E%3CAddress1%2F%3E%3CAddress2%3E6406+Ivy+Lane%3C%2FAddress2%3E%3CCity%3EGreenbelt%3C%2FCity%3E%3CState%3EMD%3C%2FState%3E%3CZip5%3E20770%3C%2FZip5%3E%3CZip4%3E1441%3C%2FZip4%3E%3C%2FAddress%3E%3C%2FAddressValidateRequest%3E"""
+    fake_urlopen = fake_urlopen.with_args(req)
+    res = StringIO("""<?xml version="1.0"?>
+<AddressValidateResponse><Address ID="0"><Address2>6406 IVY LN</Address2><City>GREENBELT</City><State>MD</State><Zip5>20770</Zip5><Zip4>1441</Zip4></Address></AddressValidateResponse>""")
+    fake_urlopen.returns(res)
+
+    addresses = [
+        OrderedDict([
+                ('address', '6406 Ivy Lane'),
+                ('city', 'Greenbelt'),
+                ('state', 'MD'),
+                ('zip_code', '20770-1441'),
+                ])
+        ]
+    res = verify(
+        'foo_id',
+        addresses,
+        )
+
+    expected = [
+        OrderedDict([
+                ('address', '6406 IVY LN'),
+                ('city', 'GREENBELT'),
+                ('state', 'MD'),
+                ('zip5', '20770'),
+                ('zip4', '1441'),
+                ])
+        ]
+    eq(res, expected)
+
+@fudge.patch('urllib2.urlopen')
+def test_verify_firm_name(fake_urlopen):
+    fake_urlopen = fake_urlopen.expects_call()
+    req = """http://production.shippingapis.com/ShippingAPI.dll?API=Verify&XML=%3CAddressValidateRequest+USERID%3D%22foo_id%22%3E%3CAddress+ID%3D%220%22%3E%3CFirmName%3EXYZ+Corp%3C%2FFirmName%3E%3CAddress1%2F%3E%3CAddress2%3E6406+Ivy+Lane%3C%2FAddress2%3E%3CCity%3EGreenbelt%3C%2FCity%3E%3CState%3EMD%3C%2FState%3E%3CZip5%2F%3E%3CZip4%2F%3E%3C%2FAddress%3E%3C%2FAddressValidateRequest%3E"""
+    fake_urlopen = fake_urlopen.with_args(req)
+    res = StringIO("""<?xml version="1.0"?>
+<AddressValidateResponse><Address ID="0"><FirmName>XYZ CORP</FirmName><Address2>6406 IVY LN</Address2><City>GREENBELT</City><State>MD</State><Zip5>20770</Zip5><Zip4>1441</Zip4></Address></AddressValidateResponse>""")
+    fake_urlopen.returns(res)
+
+    addresses = [
+        OrderedDict([
+                ('firm_name', 'XYZ Corp'),
+                ('address', '6406 Ivy Lane'),
+                ('city', 'Greenbelt'),
+                ('state', 'MD'),
+                ])
+        ]
+    res = verify(
+        'foo_id',
+        addresses,
+        )
+
+    expected = [
+        OrderedDict([
+                ('firm_name', 'XYZ CORP'),
+                ('address', '6406 IVY LN'),
+                ('city', 'GREENBELT'),
+                ('state', 'MD'),
+                ('zip5', '20770'),
+                ('zip4', '1441'),
+                ])
+        ]
+    eq(res, expected)
+
+@fudge.patch('urllib2.urlopen')
+def test_verify_address_extended(fake_urlopen):
+    fake_urlopen = fake_urlopen.expects_call()
+    req = """http://production.shippingapis.com/ShippingAPI.dll?API=Verify&XML=%3CAddressValidateRequest+USERID%3D%22foo_id%22%3E%3CAddress+ID%3D%220%22%3E%3CAddress1%3ESuite+12%3C%2FAddress1%3E%3CAddress2%3E6406+Ivy+Lane%3C%2FAddress2%3E%3CCity%3EGreenbelt%3C%2FCity%3E%3CState%3EMD%3C%2FState%3E%3CZip5%2F%3E%3CZip4%2F%3E%3C%2FAddress%3E%3C%2FAddressValidateRequest%3E"""
+    fake_urlopen = fake_urlopen.with_args(req)
+    res = StringIO("""<?xml version="1.0"?>
+<AddressValidateResponse><Address ID="0"><Address1>STE 12</Address1><Address2>6406 IVY LN</Address2><City>GREENBELT</City><State>MD</State><Zip5>20770</Zip5><Zip4>1441</Zip4></Address></AddressValidateResponse>""")
+    fake_urlopen.returns(res)
+
+    addresses = [
+        OrderedDict([
+                ('address', '6406 Ivy Lane'),
+                ('address_extended', 'Suite 12'),
+                ('city', 'Greenbelt'),
+                ('state', 'MD'),
+                ])
+        ]
+    res = verify(
+        'foo_id',
+        addresses,
+        )
+
+    expected = [
+        OrderedDict([
+                ('address_extended', 'STE 12'),
+                ('address', '6406 IVY LN'),
+                ('city', 'GREENBELT'),
+                ('state', 'MD'),
+                ('zip5', '20770'),
+                ('zip4', '1441'),
+                ])
+        ]
+    eq(res, expected)
+
+@fudge.patch('urllib2.urlopen')
+def test_verify_urbanization(fake_urlopen):
+    fake_urlopen = fake_urlopen.expects_call()
+    req = """http://production.shippingapis.com/ShippingAPI.dll?API=Verify&XML=%3CAddressValidateRequest+USERID%3D%22foo_id%22%3E%3CAddress+ID%3D%220%22%3E%3CAddress1%2F%3E%3CAddress2%3E6406+Ivy+Lane%3C%2FAddress2%3E%3CCity%3EGreenbelt%3C%2FCity%3E%3CState%3EMD%3C%2FState%3E%3CUrbanization%3EPuerto+Rico%3C%2FUrbanization%3E%3CZip5%2F%3E%3CZip4%2F%3E%3C%2FAddress%3E%3C%2FAddressValidateRequest%3E"""
+    fake_urlopen = fake_urlopen.with_args(req)
+    res = StringIO("""<?xml version="1.0"?>
+<AddressValidateResponse><Address ID="0"><Address2>6406 IVY LN</Address2><City>GREENBELT</City><State>MD</State><Urbanization>PUERTO RICO</Urbanization><Zip5>20770</Zip5><Zip4>1441</Zip4></Address></AddressValidateResponse>""")
+    fake_urlopen.returns(res)
+
+    addresses = [
+        OrderedDict([
+                ('address', '6406 Ivy Lane'),
+                ('urbanization', 'Puerto Rico'),
+                ('city', 'Greenbelt'),
+                ('state', 'MD'),
+                ])
+        ]
+    res = verify(
+        'foo_id',
+        addresses,
+        )
+
+    expected = [
+        OrderedDict([
+                ('address', '6406 IVY LN'),
+                ('city', 'GREENBELT'),
+                ('state', 'MD'),
+                ('urbanization', 'PUERTO RICO'),
+                ('zip5', '20770'),
+                ('zip4', '1441'),
+                ])
+        ]
+    eq(res, expected)
+
+@fudge.patch('urllib2.urlopen')
+def test_verify_multiple(fake_urlopen):
+    fake_urlopen = fake_urlopen.expects_call()
+    req = """http://production.shippingapis.com/ShippingAPI.dll?API=Verify&XML=%3CAddressValidateRequest+USERID%3D%22foo_id%22%3E%3CAddress+ID%3D%220%22%3E%3CAddress1%2F%3E%3CAddress2%3E6406+Ivy+Lane%3C%2FAddress2%3E%3CCity%3EGreenbelt%3C%2FCity%3E%3CState%3EMD%3C%2FState%3E%3CZip5%2F%3E%3CZip4%2F%3E%3C%2FAddress%3E%3CAddress+ID%3D%221%22%3E%3CAddress1%2F%3E%3CAddress2%3E8+Wildwood+Drive%3C%2FAddress2%3E%3CCity%3EOld+Lyme%3C%2FCity%3E%3CState%3ECT%3C%2FState%3E%3CZip5%2F%3E%3CZip4%2F%3E%3C%2FAddress%3E%3C%2FAddressValidateRequest%3E"""
+    fake_urlopen = fake_urlopen.with_args(req)
+    res = StringIO("""<?xml version="1.0"?>
+<AddressValidateResponse><Address ID="0"><Address2>6406 IVY LN</Address2><City>GREENBELT</City><State>MD</State><Zip5>20770</Zip5><Zip4>1441</Zip4></Address><Address ID="1"><Address2>8 WILDWOOD DR</Address2><City>OLD LYME</City><State>CT</State><Zip5>06371</Zip5><Zip4>1844</Zip4></Address></AddressValidateResponse>""")
+    fake_urlopen.returns(res)
+
+    addresses = [
+        OrderedDict([
+                ('address', '6406 Ivy Lane'),
+                ('city', 'Greenbelt'),
+                ('state', 'MD'),
+                ]),
+        OrderedDict([
+                ('address', '8 Wildwood Drive'),
+                ('city', 'Old Lyme'),
+                ('state', 'CT'),
+                ]),
+        ]
+    res = verify(
+        'foo_id',
+        addresses,
+        )
+
+    expected = [
+        OrderedDict([
+                ('address', '6406 IVY LN'),
+                ('city', 'GREENBELT'),
+                ('state', 'MD'),
+                ('zip5', '20770'),
+                ('zip4', '1441'),
+                ]),
+        OrderedDict([
+                ('address', '8 WILDWOOD DR'),
+                ('city', 'OLD LYME'),
+                ('state', 'CT'),
+                ('zip5', '06371'),
+                ('zip4', '1844'),
+                ]),
+        ]
+    eq(res, expected)
+
+@fudge.patch('urllib2.urlopen')
+def test_verify_more_than_5(fake_urlopen):
+    addresses = [
+        OrderedDict(),
+        OrderedDict(),
+        OrderedDict(),
+        OrderedDict(),
+        OrderedDict(),
+        OrderedDict(),
+        ]
+
+    msg = assert_raises(
+        ValueError,
+        verify,
+        'foo_id',
+        addresses,
+        )
+
+    eq(str(msg), 'Only 5 addresses are allowed per request')
+
+@fudge.patch('urllib2.urlopen')
+def test_verify_api_error(fake_urlopen):
+    fake_urlopen = fake_urlopen.expects_call()
+    req = """http://production.shippingapis.com/ShippingAPI.dll?API=Verify&XML=%3CAddressValidateRequest+USERID%3D%22foo_id%22%3E%3CAddress+ID%3D%220%22%3E%3CAddress1%2F%3E%3CAddress2%3E6406+Ivy+Lane%3C%2FAddress2%3E%3CCity%3EGreenbelt%3C%2FCity%3E%3CState%3EMD%3C%2FState%3E%3CZip5%2F%3E%3CZip4%2F%3E%3C%2FAddress%3E%3C%2FAddressValidateRequest%3E"""
+    fake_urlopen = fake_urlopen.with_args(req)
+    res = StringIO("""<Error>
+        <Number>80040b1a</Number>
+        <Description>Authorization failure.  Perhaps username and/or password is incorrect.</Description>
+        <Source>UspsCom::DoAuth</Source>
+</Error>""")
+    fake_urlopen.returns(res)
+
+    addresses = [
+        OrderedDict([
+                ('address', '6406 Ivy Lane'),
+                ('city', 'Greenbelt'),
+                ('state', 'MD'),
+                ])
+        ]
+    msg = assert_raises(
+        ValueError,
+        verify,
+        'foo_id',
+        addresses,
+        )
+
+    expected = ('80040b1a: Authorization failure.  Perhaps username '
+                'and/or password is incorrect.'
+                )
+    eq(str(msg), expected)
